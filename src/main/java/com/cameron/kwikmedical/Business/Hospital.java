f8b32a0cdd61b2ae24d1b4b053c3562ffb190c64
CamyH <cameronhunt88@hotmail.com>
2022-11-04 10:23:16 +0000

    added constructor with getters and setters

--- src/main/java/com/cameron/kwikmedical/Business/Hospital.java
+++ src/main/java/com/cameron/kwikmedical/Business/Hospital.java
@@ -1,4 +1,38 @@
 package com.cameron.kwikmedical.Business;
 
 public class Hospital {
+
+    public Hospital(String name, String address, String postCode) {
+        this.name = name;
+        this.address = address;
+        this.postCode = postCode;
+    }
+
+    private String name;
+    private String address;
+    private String postCode;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
 }
