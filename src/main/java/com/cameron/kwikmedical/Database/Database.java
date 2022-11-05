1d2c0d6d5a45f7382e66564eb84dd355d1b39095
Cameron <cameronhunt88@hotmail.com>
2022-11-05 21:14:25 +0000

    added some DB methods for connecting and querying mysql DB

--- src/main/java/com/cameron/kwikmedical/Database/Database.java
+++ src/main/java/com/cameron/kwikmedical/Database/Database.java
@@ -1,4 +1,39 @@
 package com.cameron.kwikmedical.Database;
+import java.sql.*;
 
 public class Database {
+    private Connection conn;
+    public void DBConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/kwikmedical?user=Java&password=Java");
+        } catch (Exception err) {
+            System.out.println(err.getMessage());
+        }
+    }
+
+    public void TerminateDB() throws SQLException {
+        conn.close();
+    }
+
+    public void DBInsertPatientDetails(String firstName, String lastName, Integer nhsNumber, String address, String postCode, String medicalCond) {
+        try {
+            // Setting up PreparedStatement to insert patient details into DB
+            Statement statement = conn.createStatement();
+            String query = "INSERT INTO PatientDetails (FirstName, LastName, NHSNumber, Address, PostCode, MedicalCond) VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setInt(3, nhsNumber);
+            preparedStatement.setString(4, address);
+            preparedStatement.setString(5, postCode);
+            preparedStatement.setString(6, medicalCond);
+
+            // Execute insertion query
+            preparedStatement.executeQuery();
+
+        } catch (Exception err) {
+            System.out.println(err.getMessage());
+        }
+    }
 }
