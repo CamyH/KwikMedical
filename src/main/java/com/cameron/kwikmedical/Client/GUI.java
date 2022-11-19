1f9c596db226e5706f38997803a5f0b001712532
Cameron <cameronhunt88@hotmail.com>
2022-11-19 12:07:13 +0000

    removed unused code

--- src/main/java/com/cameron/kwikmedical/Client/GUI.java
+++ src/main/java/com/cameron/kwikmedical/Client/GUI.java
@@ -1,14 +1,4 @@
 package com.cameron.kwikmedical.Client;
-import com.cameron.kwikmedical.Business.*;
-import com.cameron.kwikmedical.Database.Database;
-
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import javax.swing.*;
 
 /**
  * Class for calling GUI
